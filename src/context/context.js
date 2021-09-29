5fa9f6c8554c22b0b9377cdf37a2343e4e2473ab
Oam surachet <surachat2014@gmail.com>
2021-09-29 09:28:03 +0700

    add linter extention

---
+++ src/context/context.js
@@ -0,0 +1,30 @@
+import React, { cloneElement } from "react";
+import { ProductsProvider } from "./products_context";
+import { FilterProvider } from "./filter_context";
+import { CartProvider } from "./cart_context";
+import { UserProvider } from "./user_context";
+
+function ProviderComposer({ contexts, children }) {
+  return contexts.reduce(
+    (kids, parent) =>
+      cloneElement(parent, {
+        children: kids,
+      }),
+    children
+  );
+}
+
+export default function AppContextProvider({ children }) {
+  return (
+    <ProviderComposer
+      contexts={[
+        <CartProvider key={0} />,
+        <FilterProvider key={1} />,
+        <ProductsProvider key={2} />,
+        <UserProvider key={3} />,
+      ]}
+    >
+      {children}
+    </ProviderComposer>
+  );
+}
